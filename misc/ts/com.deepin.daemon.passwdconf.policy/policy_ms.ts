1b03e6b9ab6346186a5bf734fe3a2ecf6021d606
dengbo <dengbo@uniontech.com>
2022-07-24 17:22:36 +0800

    fix: 更新翻译
    更新翻译文案
    
    Log: 更新翻译
    Influence: 登录界面密码校验文案提示
    Bug: https://pms.uniontech.com/bug-view-149113.html
    Change-Id: I7da113ce388f36ad453f9c58e20791ee1a755231

--- misc/ts/com.deepin.daemon.passwdconf.policy/policy_ms.ts
+++ misc/ts/com.deepin.daemon.passwdconf.policy/policy_ms.ts
@@ -2,44 +2,14 @@
 	<context>
 		<name>policy</name>
 		<message>
-			<location filename="com.deepin.daemon.authenticate.Fingerprint.enroll!message" line="0"/>
-			<source>Authentication is required to add fingerprint password</source>
-			<translation>Pengesahihan diperlukan untuk menambah kata laluan cap jari</translation>
+			<location filename="com.deepin.daemon.passwdconf.modify-config!message" line="0"/>
+			<source>Authentication is required to modify the configuration for password validation</source>
+			<translation>Pengesahihan diperlukan untuk mengubah suai konfigurasi untuk pengesahan kata laluan</translation>
 		</message>
 		<message>
-			<location filename="com.deepin.daemon.authenticate.Fingerprint.enroll!description" line="0"/>
-			<source>Add fingerprint password</source>
-			<translation>Tambah kata laluan cap jari</translation>
-		</message>
-		<message>
-			<location filename="com.deepin.daemon.authenticate.Fingerprint.delete-enrolled-fingers!message" line="0"/>
-			<source>Authentication is required to clear fingerprint passwords</source>
-			<translation>Pengesahihan diperlukan untuk mengosongkan kata laluan cap jari</translation>
-		</message>
-		<message>
-			<location filename="com.deepin.daemon.authenticate.Fingerprint.delete-enrolled-fingers!description" line="0"/>
-			<source>Clear fingerprint passwords</source>
-			<translation>Kosongkan kata laluan cap jari</translation>
-		</message>
-		<message>
-			<location filename="com.deepin.daemon.authenticate.Fingerprint.rename-enrolled-finger!message" line="0"/>
-			<source>Authentication is required to rename fingerprint password</source>
-			<translation>Pengesahihan diperlukan untuk menamakan semula kata laluan cap jari</translation>
-		</message>
-		<message>
-			<location filename="com.deepin.daemon.authenticate.Fingerprint.rename-enrolled-finger!description" line="0"/>
-			<source>Rename fingerprint password</source>
-			<translation>Namakan semula kata laluan cap jari</translation>
-		</message>
-		<message>
-			<location filename="com.deepin.daemon.authenticate.Fingerprint.manage!message" line="0"/>
-			<source>Password is required to perform this action</source>
-			<translation>Kata laluan diperlukan untuk membuat tindakan ini</translation>
-		</message>
-		<message>
-			<location filename="com.deepin.daemon.authenticate.Fingerprint.manage!description" line="0"/>
-			<source>Manage fingerprint passwords</source>
-			<translation>Urus kata laluan cap jari</translation>
+			<location filename="com.deepin.daemon.passwdconf.modify-config!description" line="0"/>
+			<source>Modify</source>
+			<translation>Ubah Suai</translation>
 		</message>
 	</context>
 </TS>
